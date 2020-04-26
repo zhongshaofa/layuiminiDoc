0b05e96eb1f76e58e4618b72a3ac4583cde00407
chung <chung@99php.cn>
2020-04-26 14:57:18 +0800

    修改文档

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-grep -rl '^src="../gitbook' ./ | xargs sed -i 's/src="../gitbook.*/src="layuimini.oss-cn-shenzhen.aliyuncs.com/public/uploads/gitbook/g'
-grep -rl '^href="../gitbook' ./ | xargs sed -i 's/href="../gitbook.*/href="layuimini.oss-cn-shenzhen.aliyuncs.com/public/uploads/gitbook/g'
-grep -rl '^src="gitbook' ./ | xargs sed -i 's/src="gitbook.*/src="layuimini.oss-cn-shenzhen.aliyuncs.com/public/uploads/gitbook/g'
-grep -rl '^href="gitbook' ./ | xargs sed -i 's/href="gitbook.*/href="layuimini.oss-cn-shenzhen.aliyuncs.com/public/uploads/gitbook/g'
+grep -rl 'src="../gitbook'  _book | xargs sed -i 's/src="..\/gitbook/src="https:\/\/layuimini.oss-cn-shenzhen.aliyuncs.com\/public\/uploads\/gitbook/g'
+grep -rl 'href="../gitbook' _book | xargs sed -i 's/href="..\/gitbook/href="https:\/\/layuimini.oss-cn-shenzhen.aliyuncs.com\/public\/uploads\/gitbook/g'
+grep -rl 'src="gitbook' _book | xargs sed -i 's/src="gitbook/src="https:\/\/layuimini.oss-cn-shenzhen.aliyuncs.com\/public\/uploads\/gitbook/g'
+grep -rl 'href="gitbook' _book | xargs sed -i 's/href="gitbook/href="https:\/\/layuimini.oss-cn-shenzhen.aliyuncs.com\/public\/uploads\/gitbook/g'
